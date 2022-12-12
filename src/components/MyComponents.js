5b5bc2046155e07ce2c7ac1d3979549237917bd3
Võ Linh Bảo <vlb@netcompany.com>
2022-12-12 11:04:51 +0700

    add 2 buttons

--- src/components/MyComponents.js
+++ src/components/MyComponents.js
@@ -1,10 +1,27 @@
 import React from "react";
 
 class MyComponent extends React.Component {
+    
+    state = {
+        name: 'Bao',
+        address: 'Vietnam',
+        age: 22
+    };
+
+    handleClick(event){
+        console.log(this.state.name)
+        // console.log(event)
+    }
+
+    handleOnMouseOver(event){
+        console.log(event)
+    }
+
     render(){
         return(
-            <div>My first component
-                {Math.random()}
+            <div>My name is {this.state.name}, I'm from {this.state.address}
+            <button onClick={this.handleClick}>Click me</button>
+            <button onMouseOver={this.handleOnMouseOver}>Hover me</button>
             </div>
         )
     }
